c80c0abb975abe8f0078aa0f0ab7f02ef46a935d
Silver Kuusik <silver.kuusik@gmail.com>
2018-06-06 23:54:47 +0300

    add showing usb connecting status

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ Last edited: 18th May 2018
 import os
 import sys
 import json
+import time
 import serial
 import _thread
 import serial.tools.list_ports
@@ -35,7 +36,9 @@ RESOURCE_PATH = 'res'
 if hasattr(sys, '_MEIPASS'):
     RESOURCE_PATH = os.path.join(sys._MEIPASS, RESOURCE_PATH)
 
-class SumoManager(QWidget):
+class SumoManager(QMainWindow):
+    selectedSerialport = ''
+    addWifiDisabled = False
 
     def __init__(self):
         super().__init__()
@@ -44,49 +47,56 @@ class SumoManager(QWidget):
     def initUI(self):
         # load the Orbitron font
         QFontDatabase.addApplicationFont(os.path.join(RESOURCE_PATH, 'orbitron.ttf'))
-        # message label
-        self.messageLabel = QLabel()
+
         # logo
         logoLabel = QLabel()
         logoLabel.setPixmap(QPixmap(os.path.join(RESOURCE_PATH, 'sumologo.svg')))
         logoLabel.setAlignment(Qt.AlignCenter)
 
         # serial port selection field
-        serialLabel = QLabel('1. Select serial port')
+        serialNoticeLabel = QLabel('1. Connect SumoRobot via USB')
+        serialNoticeLabel.setStyleSheet('margin-top: 20px;')
         # update serial ports every X seconds
-        self.serialBox = QComboBox()
-        # make serialBox readonly
+        self.serialport = QLabel()
+        self.serialport.setPixmap(QPixmap(os.path.join(RESOURCE_PATH, 'usb.png')))
 
         # WiFi credentials fields
         wifiLabel = QLabel('2. Enter WiFi credentials')
+        wifiLabel.setStyleSheet('margin-top: 20px;')
         self.wifiNameEdit = QLineEdit()
-        self.wifiNameEdit.setPlaceholderText("WiFi SSID")
+        self.wifiNameEdit.setPlaceholderText("Network")
         self.wifiPwdEdit = QLineEdit()
-        self.wifiPwdEdit.setPlaceholderText("WiFi Password")
+        self.wifiPwdEdit.setPlaceholderText("Password")
 
         # WiFi add button
         addWifiLabel = QLabel('3. Click add Wifi network')
-        addWifiBtn = QPushButton('Add WiFi network', self)
-        addWifiBtn.setCursor(QCursor(Qt.PointingHandCursor))
-        addWifiBtn.clicked.connect(self.buttonClicked)
+        addWifiLabel.setStyleSheet('margin-top: 20px;')
+        self.addWifiBtn = QPushButton('Add WiFi network', self)
+        self.addWifiBtn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.addWifiBtn.clicked.connect(self.buttonClicked)
 
-        # app layout
+        # vertical app layout
         vbox = QVBoxLayout()
         vbox.addWidget(logoLabel)
-        vbox.addWidget(self.messageLabel)
-        vbox.addWidget(serialLabel)
-        vbox.addWidget(self.serialBox)
+        vbox.addWidget(serialNoticeLabel)
+        vbox.addWidget(self.serialport)
         vbox.addWidget(wifiLabel)
         vbox.addWidget(self.wifiNameEdit)
         vbox.addWidget(self.wifiPwdEdit)
         vbox.addWidget(addWifiLabel)
-        vbox.addWidget(addWifiBtn)
+        vbox.addWidget(self.addWifiBtn)
+        # wrap the layout into a widget
+        mainWidget = QWidget()
+        mainWidget.setLayout(vbox)
+
+        # show a blank message to expand the window
+        self.showMessage('info', '')
 
         # main window style, layout and position
         with open(os.path.join(RESOURCE_PATH, 'main.qss'), 'r') as file:
             self.setStyleSheet(file.read())
         self.setWindowTitle('SumoManager' + VERSION)
-        self.setLayout(vbox)
+        self.setCentralWidget(mainWidget)
         self.show()
         self.center()
 
@@ -100,100 +110,114 @@ class SumoManager(QWidget):
 
     def showMessage(self, type, message):
         if type == 'error':
-            self.messageLabel.setStyleSheet('QLabel {color:#d9534f}')
+            self.statusBar().setStyleSheet('color: #d9534f; background: rgba(212,212,255,0.035); font-family: Orbitron;')
         elif type == 'warning':
-            self.messageLabel.setStyleSheet('QLabel {color:#f0ad4e;}')
+            self.statusBar().setStyleSheet('color: #f0ad4e; background: rgba(212,212,255,0.035); font-family: Orbitron;')
         elif type == 'info':
-            self.messageLabel.setStyleSheet('QLabel {color:#5cb85c;}')
+            self.statusBar().setStyleSheet('color: #5cb85c; background: rgba(212,212,255,0.035); font-family: Orbitron;')
         else:
             # unrecognized message type
             return
-        self.messageLabel.setText(message)
+        self.statusBar().showMessage(message)
 
     # button clicked event
     def buttonClicked(self):
-        port = self.serialBox.currentText()
+        port = self.selectedSerialport
         ssid = self.wifiNameEdit.text()
         pwd = self.wifiPwdEdit.text()
 
-        # check the input fields and give the user feedback
-        if port == '':
-            self.serialBox.setStyleSheet('background-color: #d9534f;')
+        # when button disabled
+        if self.addWifiDisabled:
             return
-        else:
-            self.serialBox.setStyleSheet('background: rgba(212,212,255,0.035);')
+        # when ESP not connected
+        if port == '':
+            return
+        # when the network name is not valid
         if ssid == '':
+            # show the error
             self.wifiNameEdit.setStyleSheet('background-color: #d9534f;')
             return
+        # when the network name is valid
         else:
+            # remove the error
             self.wifiNameEdit.setStyleSheet('background: rgba(212,212,255,0.035);')
 
+        # disable the button
+        self.addWifiDisabled = True
+        # show the user feedback that we started to add the network
         self.showMessage('info', 'Adding WiFi credentials ...')
 
+        # function to update the config file on the ESP
         def updateConfig():
             # open the serial port
             board = Files(Pyboard(port))
-            # get the config file
-            config = json.loads(board.get('config.json'))
-            # add the WiFi credentials
-            config['wifis'][ssid] = pwd
-            # convert the json object into a string
-            config = json.dumps(config, indent = 8)
-            # write the updates config file
-            board.put('config.json', config)
-            self.showMessage('info', 'WiFi credentials successfully added')
 
+            tries = 5
+            config = ''
+            # while we have the config
+            while not config:
+                # only try X times
+                if tries == 0:
+                    self.showMessage('error', 'Failed to read config file')
+                    return
+                # try to read and parse config file
+                try:
+                    config = json.loads(board.get('config.json'))
+                    if config:
+                        # add the WiFi credentials
+                        config['wifis'][ssid] = pwd
+                        # convert the json object into a string
+                        config = json.dumps(config, indent = 8)
+                        # write the updates config file
+                        board.put('config.json', config)
+                except:
+                    pass
+                # one less try
+                tries -= 1
+                # wait before trying again
+                time.sleep(1)
+
+            # enable the button
+            self.addWifiDisabled = False
+            # should improve to signals and slots
+            self.statusBar().showMessage('WiFi credentials successfully added')
+
+        # start the config update thread
         _thread.start_new_thread(updateConfig, ())
 
     # when mouse clicked clear the focus on the input fields
     def mousePressEvent(self, event):
         self.wifiNameEdit.clearFocus()
         self.wifiPwdEdit.clearFocus()
-        self.serialBox.clearFocus()
-        QMainWindow.mousePressEvent(self, event)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = SumoManager()
+    # for high dpi displays
+    app.setAttribute(Qt.AA_UseHighDpiPixmaps)
+    # create the app main window
+    window = SumoManager()
 
-    serial_len = 0
-    remove_warning = False
-    def updateSerialBox():
-        global serial_len
-        global remove_warning
-
-        serial_ports = []
-        # scan the serial ports with specific vendor ID
+    # to update serialport status
+    def updateSerialport():
+        # scan the serialports with specific vendor ID
         for port in serial.tools.list_ports.comports():
             if '1A86:' in port.hwid:
-                serial_ports.append(port.device)
+                # only update if port has changed
+                if window.selectedSerialport != port.device:
+                    window.selectedSerialport = port.device
+                    window.showMessage('info', 'SumoRobot connected')
+                    window.serialport.setPixmap(QPixmap(os.path.join(RESOURCE_PATH, 'usb_connected.png')))
+                # only add the first found serialport
+                return
 
-        # when there are no serial ports connected
-        if len(serial_ports) == 0:
-            ex.showMessage('warning', 'Please connect your SumoRobot')
-            remove_warning = True
-        elif remove_warning:
-            ex.serialBox.setStyleSheet('background: rgba(212,212,255,0.035);')
-            ex.showMessage('warning', '')
-            remove_warning = False
+        # when no serial port with the specific vendor ID was found
+        window.selectedSerialport = ''
+        window.serialport.setPixmap(QPixmap(os.path.join(RESOURCE_PATH, 'usb.png')))
+        window.showMessage('warning', 'Please connect your SumoRobot')
 
-        # only update if ports changed
-        if serial_len == len(serial_ports):
-            return
-
-        # update serial ports length
-        serial_len = len(serial_ports)
-
-        # clear the serial ports
-        ex.serialBox.clear()
-
-        # list all serial ports
-        for port in serial_ports:
-            ex.serialBox.addItem(port)
-
-    # update serial ports every X second
+    # update serial port status every X second
     timer = QTimer()
-    timer.timeout.connect(updateSerialBox)
+    timer.timeout.connect(updateSerialport)
     timer.start(1000)
 
     sys.exit(app.exec_())
